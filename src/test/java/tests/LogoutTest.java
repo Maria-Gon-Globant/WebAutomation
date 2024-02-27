6a304219f41d1b0160ced53638fc1a3be7a9f51f
Maria-Gon-Globant <m.gonzalezescallon@globant.com>
2024-02-27 17:28:06 -0500

    Add: Assertion and parameters

--- src/test/java/tests/LogoutTest.java
+++ src/test/java/tests/LogoutTest.java
@@ -1,11 +1,12 @@
 package tests;
 
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 import pages.LoginPage;
 import pages.LogoutPage;
 import utils.baseTest.BaseTest;
+import static org.testng.Assert.assertTrue;
 
 public class LogoutTest extends BaseTest {
 
@@ -18,10 +19,13 @@ public class LogoutTest extends BaseTest {
     }
 
     @Test
-    public void Logout(){
+    @Parameters({"loginTitle"})
+    public void Logout(String title){
         LogoutPage logoutPage = new LogoutPage(getDriver());
         logoutPage.clickBurgerMenuButton();
         logoutPage.getImplicitWait();
         logoutPage.clickLogoutButton();
+
+        assertTrue(logoutPage.areInLogin(title), "The title does not match");
     }
 }
