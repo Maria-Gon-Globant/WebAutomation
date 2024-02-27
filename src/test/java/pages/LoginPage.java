cf927b381974e483037c6e04d047241cd1e5da66
Maria-Gon-Globant <m.gonzalezescallon@globant.com>
2024-02-27 00:35:06 -0500

    Add: LoginPage constructor

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -1,8 +1,14 @@
 package pages;
 
-public class LoginPage {
+import org.openqa.selenium.WebDriver;
+import utils.basePage.BasePage;
+
+public class LoginPage extends BasePage {
     private static final String usernameText = "standard_user";
     private static final String passwordText = "secret_sauce";
 
+    public LoginPage(WebDriver driver) {
+        super(driver);
+    }
 
 }
