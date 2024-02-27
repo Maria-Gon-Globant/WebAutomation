76bc8c94b0999974a1c51d8bff256611dfab527f
Maria-Gon-Globant <m.gonzalezescallon@globant.com>
2024-02-27 00:57:05 -0500

    Add: AddItemPage constructor

---
+++ src/test/java/pages/AddItemPage.java
@@ -0,0 +1,10 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import utils.basePage.BasePage;
+
+public class AddItemPage extends BasePage {
+    public AddItemPage(WebDriver driver) {
+        super(driver);
+    }
+}
