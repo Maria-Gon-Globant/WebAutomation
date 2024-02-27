0fa3c1937cab1bde663fd8b5857bc7f821a54d68
Maria-Gon-Globant <m.gonzalezescallon@globant.com>
2024-02-27 00:40:44 -0500

    Add: LogoutPage constructor

---
+++ src/test/java/pages/LogoutPage.java
@@ -0,0 +1,11 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import utils.basePage.BasePage;
+
+public class LogoutPage extends BasePage {
+
+    public LogoutPage(WebDriver driver) {
+        super(driver);
+    }
+}
