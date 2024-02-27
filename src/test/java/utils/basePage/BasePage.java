bbdc4d62ff96755344e627d256f8e58a7e430f94
Maria-Gon-Globant <m.gonzalezescallon@globant.com>
2024-02-27 16:19:05 -0500

    Add: Explicit wait

--- src/test/java/utils/basePage/BasePage.java
+++ src/test/java/utils/basePage/BasePage.java
@@ -1,7 +1,9 @@
 package utils.basePage;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
@@ -20,7 +22,9 @@ public class BasePage {
     public WebDriver getDriver() {
         return driver;
     }
-
+    public void waitElementVisibility(WebElement element) {
+        getWait().until(ExpectedConditions.visibilityOf(element));
+    }
     public WebDriverWait getWait() {
         return wait;
     }
