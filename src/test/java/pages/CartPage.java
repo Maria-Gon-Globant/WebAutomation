5111160e73c20b23ff74815e375eb2d14a81040a
Maria-Gon-Globant <m.gonzalezescallon@globant.com>
2024-02-27 01:25:52 -0500

    Add: CartPage constructor

---
+++ src/test/java/pages/CartPage.java
@@ -0,0 +1,10 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import utils.basePage.BasePage;
+
+public class CartPage extends BasePage {
+    public CartPage(WebDriver driver) {
+        super(driver);
+    }
+}
