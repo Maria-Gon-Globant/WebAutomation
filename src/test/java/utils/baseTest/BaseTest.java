da8331ccd7aa8926b6f8d1892b0961a01a850e61
Maria-Gon-Globant <m.gonzalezescallon@globant.com>
2024-02-27 00:09:49 -0500

    Add: After method in Base Test

--- src/test/java/utils/baseTest/BaseTest.java
+++ src/test/java/utils/baseTest/BaseTest.java
@@ -2,6 +2,7 @@ package utils.baseTest;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
 import utils.MyDriver;
@@ -14,6 +15,11 @@ public class BaseTest {
     public void googleSetUp(String url){
         driver = new MyDriver();
         driver.getDriver().navigate().to(url);
+    }
+
+    @AfterMethod()
+    public void closeGoogle() {
+        driver.getDriver().close();
     }
 
 }
