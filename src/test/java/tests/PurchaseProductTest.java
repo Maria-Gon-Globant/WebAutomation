844e0b4fa70ae51430be0294d2374823773a479f
Maria-Gon-Globant <m.gonzalezescallon@globant.com>
2024-02-27 19:23:51 -0500

    Add: Purchase items test

--- src/test/java/tests/PurchaseProductTest.java
+++ src/test/java/tests/PurchaseProductTest.java
@@ -1,8 +1,14 @@
 package tests;
 
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import pages.CartPage;
+import pages.ItemPage;
 import pages.LoginPage;
 import utils.baseTest.BaseTest;
+
+import static org.testng.Assert.assertTrue;
 
 public class PurchaseProductTest extends BaseTest {
 
@@ -12,5 +18,28 @@ public class PurchaseProductTest extends BaseTest {
         loginPage.sendUsername();
         loginPage.sendPassword();
         loginPage.clickLoginButton();
+    }
+
+    @Test
+    @Parameters({"successText"})
+    public void PurchaseProduct(String text){
+        ItemPage itemPage = new ItemPage(getDriver());
+        itemPage.clickItem1();
+        itemPage.clickItem2();
+        itemPage.clickItem3();
+
+        CartPage cartPage = itemPage.clickCartButton();
+
+        cartPage.clickCheckoutButton();
+
+        cartPage.sendName();
+        cartPage.sendLastName();
+        cartPage.sendZip();
+
+        cartPage.clickContinueButton();
+        cartPage.clickFinishButton();
+
+        assertTrue(cartPage.successfulPurchase(text), "The purchase was not successful");
+
     }
 }
