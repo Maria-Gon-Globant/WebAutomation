b822f4d72be58b92af6bbe7337e2f38ce349ddcb
Maria-Gon-Globant <m.gonzalezescallon@globant.com>
2024-02-27 19:08:43 -0500

    Add: Remove item test

--- src/test/java/tests/RemoveItemTest.java
+++ src/test/java/tests/RemoveItemTest.java
@@ -1,10 +1,15 @@
 package tests;
 
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pages.CartPage;
+import pages.ItemPage;
 import pages.LoginPage;
 import utils.baseTest.BaseTest;
+import static org.testng.Assert.assertEquals;
 
 public class RemoveItemTest extends BaseTest {
+    private static final int numItems = 3;
 
     @BeforeMethod
     public void Login(){
@@ -12,5 +17,24 @@ public class RemoveItemTest extends BaseTest {
         loginPage.sendUsername();
         loginPage.sendPassword();
         loginPage.clickLoginButton();
+    }
+
+    @Test
+    public void RemoveItem(){
+        ItemPage itemPage = new ItemPage(getDriver());
+        itemPage.clickItem1();
+        itemPage.clickItem2();
+        itemPage.clickItem3();
+
+        CartPage cartPage = itemPage.clickCartButton();
+
+        itemPage.clickRemoveItem1();
+        itemPage.clickRemoveItem2();
+        itemPage.clickRemoveItem3();
+
+        itemPage.getImplicitWait();
+
+        assertEquals(cartPage.getSize(),numItems);
+
     }
 }
